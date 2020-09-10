b505bcb8e734818874f04b672e833ef6d54a5757
Nikita <nikita@TM-ND0028.local>
2020-09-10 07:50:34 -0700

    Created nav  page

--- src/utils/generateUrl.js
+++ src/utils/generateUrl.js
@@ -1,5 +1,5 @@
 const generateUrl = (options) => {
-    let url = 'http://www.omdbapi.com/?apikey=4f5707cc';
+    let url = 'https://www.omdbapi.com/?apikey=4f5707cc';
     if(!options || !Object.keys(options).length) return;
   
     Object.keys(options).forEach((item) => {
